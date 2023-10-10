7f861b579c2fa481682645189595532d0a552ba2
daniel-iroka <danieliroka67@gmail.com>
2023-10-10 13:23:39 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Titans_Hockey_Challenge"
+include(":app")
+ 
