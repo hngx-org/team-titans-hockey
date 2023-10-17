ebac58742aa095db1397245a7cc168c47dfd553f
Omolara Idowu <70015447+OmolaraIdowu@users.noreply.github.com>
2023-10-17 11:11:41 +0100

    Implement Play and Pause button (#8)
    * Refactor code
    
    * - Implement Pause/Play button

--- app/src/main/java/com/example/titans_hockey_challenge/models/HockeyTable.kt
+++ app/src/main/java/com/example/titans_hockey_challenge/models/HockeyTable.kt
@@ -405,14 +405,14 @@ class HockeyTable : SurfaceView, SurfaceHolder.Callback {
         return mPaddle!!.bounds.contains(event.x, event.y)
     }
 
-    private fun playStartGameSound() {
+    fun playStartGameSound() {
         if (mediaPlayer == null) {
             mediaPlayer = MediaPlayer.create(mContext, R.raw.soundtrack2)
         }
         mediaPlayer?.start()
     }
 
-    private fun pauseBackgroundSound() {
+    fun pauseBackgroundSound() {
         if (mediaPlayer != null && mediaPlayer!!.isPlaying) {
             mediaPlayer?.pause()
         }
